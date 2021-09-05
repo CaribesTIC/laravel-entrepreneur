f8e6403175e4e26920a9b41fb49b80a94538e925
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2021-09-04 23:17:02 -0400

    feat: general changes of JWT branch

--- httpd/src/app/Http/Controllers/AuthController.php
+++ httpd/src/app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
@@ -14,7 +16,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
     /**
@@ -26,6 +28,13 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
 
+        Validator::make($credentials, [   
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ])->validate();
+
+
+    
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
@@ -80,4 +89,5 @@ class AuthController extends Controller
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
     }
+
 }
--- httpd/src/routes/api.php
+++ httpd/src/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ProductController;
 
 
@@ -19,53 +20,21 @@ use App\Http\Controllers\ProductController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('auth:api')->prefix('auth')->group(function () {
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
-// Route::group([
-
-//     'middleware' => 'api',
-//     'prefix' => 'auth'
-
-// ], function ($router) {
-
-    // Route::post('login', 'AuthController@login');
-    // Route::post('logout', 'AuthController@logout');
-    // Route::post('refresh', 'AuthController@refresh');
-    // Route::post('me', 'AuthController@me');
-
-// });
-
-
-Route::middleware('api')->prefix('auth')->group(function () {
-
-    Route::post('login',    [AuthController::class, 'login']);
     Route::post('logout',   [AuthController::class, 'logout']);
-    Route::post('refresh',  [AuthController::class, 'refresh']);
-    Route::post('me',       [AuthController::class, 'me']);
-
-    Route::get('users', [UserController::class, 'index'])->name('users.index');
-
-});
-
-/*Route::middleware('api')->group(function () {
-    Route::get('products', fn() => [
-        [ "id" => 1, "title" => "iPad 4 Mini"           , "price" => 500.01, "inventory" => 2 , "photo" => "cafe.jpg"    ],
-        [ "id" => 2, "title" => "H&M T-Shirt White"     , "price" => 10.99 , "inventory" => 10, "photo" => "cebolla.jpg" ],
-        [ "id" => 3, "title" => "Charli XCX - Sucker CD", "price" => 19.99 , "inventory" => 5 , "photo" => "patata.jpg"  ],
-        [ "id" => 4, "title" => "Xharli XXX - Xucker CD", "price" => 29.99 , "inventory" => 3 , "photo" => "pina.jpg"    ],
-        [ "id" => 5, "title" => "oPad 4 oini"           , "price" => 500.01, "inventory" => 2 , "photo" => "cafe.jpg"    ],
-        [ "id" => 6, "title" => "R&M R-Shirt White"     , "price" => 10.99 , "inventory" => 10, "photo" => "cebolla.jpg" ],
-        [ "id" => 7, "title" => "Aharli ACX - Aucker CD", "price" => 19.99 , "inventory" => 5 , "photo" => "patata.jpg"  ],
-        [ "id" => 8, "title" => "Zharli ZXX - Xucker CD", "price" => 29.99 , "inventory" => 3 , "photo" => "pina.jpg"    ]
-    ]);
-});*/
-
-Route::middleware('api')->group(function () {
     Route::get('products', [ProductController::class, 'index'])->name('products.index');
+
 });
 
-// 
+
+Route::middleware('guest:api')->group(function () {
+    Route::post('login',    [AuthController::class, 'login']);
+    Route::post('register', [RegisterController::class, 'index']);
+
+});
+
+
+Route::fallback(function(){
+    abort(403, 'Unauthorized action.');
+});
